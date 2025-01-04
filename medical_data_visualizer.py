80850a05e81684f044a389cd21d521cff265f2b4
James Amos <james.amos7@gmail.com>
2025-01-03 19:45:54 -0500

    initial commit with partial solution

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,22 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+bmi = (df["weight"] / (df["height"] / 100) ** 2)
+df['overweight'] = bmi.map(lambda x: 0 if x < 25 else 1)
 
 # 3
-
+df["cholesterol"] = df["cholesterol"].map(lambda x: 0 if x == 1 else 1)
+df["gluc"] = df["gluc"].map(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    value_vars = [
+        "cholesterol",
+        "gluc",
+        "smoke",
+        "alco",
+        "active",
+        "overweight",
+    ]
+    df_cat = pd.melt(df, value_vars=value_vars)
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby("variable")
     
 
     # 7
